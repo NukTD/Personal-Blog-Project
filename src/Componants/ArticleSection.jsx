27188ecc30046cc21391638b6d54d5327aa437ad
Thanate <nuk.thanate.d@gmail.com>
2024-10-31 22:00:00 +0700

    feat:create footer and article section componant

---
+++ src/Componants/ArticleSection.jsx
@@ -0,0 +1,74 @@
+import { Search } from "lucide-react";
+
+function ArticleSection() {
+  return (
+    <>
+      <p className="text-3xl text-[#26231E] font-bold mx-6 md:ml-28">
+        Latest articles
+      </p>
+      <div className=" bg-[#EFEEEB] py-4 mt-5 md:mx-28 md:flex md:items-center md:rounded-xl md:justify-between">
+        <div className="md:ml-10 hidden md:block">
+          {/* Button */}
+          <button
+            className="md:text-[#75716B] md:hover:bg-[#DAD6D1] transition duration-300 p-4 rounded-xl
+          md:hover:text-[#43403B] md:mr-6"
+          >
+            Highlight
+          </button>
+          <button
+            className="md:text-[#75716B] md:hover:bg-[#DAD6D1] transition duration-300 p-4 rounded-xl
+          md:hover:text-[#43403B] md:mr-6"
+          >
+            Cat
+          </button>
+          <button
+            className="md:text-[#75716B] md:hover:bg-[#DAD6D1] transition duration-300 p-4 rounded-xl
+          md:hover:text-[#43403B] md:mr-6"
+          >
+            Inspiration
+          </button>
+          <button
+            className="md:text-[#75716B] md:hover:bg-[#DAD6D1] transition duration-300 p-4 rounded-xl
+          md:hover:text-[#43403B] md:mr-6"
+          >
+            General
+          </button>
+        </div>
+
+        <div className="mx-6 w-20 md:w-auto relative ">
+          {/* Search Input */}
+          <input
+            type="text"
+            placeholder="Search"
+            className="text-lg  w-80 md:w-60 h-12 rounded-md border border-[#DAD6D1] pl-4"
+          />
+          <Search className="absolute -right-56 md:right-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+        </div>
+        <p className="mx-6 mt-5 mb-2 text-[#75716B] font-medium text-lg md:hidden ">
+          Category
+        </p>
+        <div className="mx-6 md:hidden">
+          <select
+            name="select"
+            className="text-lg  w-80 h-12 rounded-md border border-[#DAD6D1] pl-4"
+          >
+            <option value="Highlight" className="text-[#75716B]">
+              Highlight
+            </option>
+            <option value="Cat" className="text-[#75716B]">
+              Cat
+            </option>
+            <option value="Inspiration" className="text-[#75716B]">
+              Inspiration
+            </option>
+            <option value="General" className="text-[#75716B]">
+              General
+            </option>
+          </select>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ArticleSection;
